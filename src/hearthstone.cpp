e616c8c5b40b92b303db969e7440358e191f26c9
Steven Schmid <steschmid@gmx.ch>
2014-05-06 13:00:20 +0200

    make detection more resilient

--- src/hearthstone.cpp
+++ src/hearthstone.cpp
@@ -26,33 +26,15 @@ QPixmap Hearthstone::Capture(int vx, int vy, int vw, int vh) {
   int virtualCanvasWidth = VIRTUAL_CANVAS_WIDTH;
   int virtualCanvasHeight = VIRTUAL_CANVAS_HEIGHT;
 
-  float scaleRatioX = (float)realCanvasWidth / virtualCanvasWidth;
-  float scaleRatioY = (float)realCanvasHeight / virtualCanvasHeight;
-
   float dx = (vx - virtualCanvasWidth/2);
   float dy = (vy - virtualCanvasHeight/2);
+  float scale = (float)realCanvasHeight / virtualCanvasHeight;
 
-  float realCanvasAspectRatio = (float)realCanvasWidth / realCanvasHeight;
-  float virtualCanvasAspectRatio = (float)virtualCanvasWidth / virtualCanvasHeight;
-
-  w = vw * scaleRatioX;
-  h = vh * scaleRatioY;
-
-  float canvasRatio = virtualCanvasAspectRatio / realCanvasAspectRatio;
-
-  if(canvasRatio < 1.0f) {
-    // width relative to height is increased compared to the virtual canvas, meaning
-    // the image is squeezed in x
-    dx *= canvasRatio;
-    w *= canvasRatio;
-  } else if(canvasRatio > 1.0f) {
-    // same but height is squeezed in y
-    dy *= canvasRatio;
-    h *= canvasRatio;
-  }
-
-  x = realCanvasWidth/2 + dx * scaleRatioX;
-  y = realCanvasHeight/2 + dy * scaleRatioY;
+  // Rounding here is important for dhash calc
+  x = roundf(realCanvasWidth/2 + dx * scale);
+  y = roundf(realCanvasHeight/2 + dy * scale);
+  w = roundf(vw * scale);
+  h = roundf(vh * scale);
 
   return capture->Capture(x, y, w, h);
 }
