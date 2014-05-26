1abef9931c6e15e13cc801f56c1cbf7a6a9cdf0f
Steven Schmid <steschmid@gmx.ch>
2014-05-27 00:30:20 +0200

    increase reliability of outcome detection

--- src/scenes/ingame_scene.h
+++ src/scenes/ingame_scene.h
@@ -17,8 +17,12 @@ public:
     ADD_GENERATED_MARKER("ingame", INGAME_ID);
     ADD_GENERATED_MARKER("going_first", INGAME_MULLIGAN_1ST_ID);
     ADD_GENERATED_MARKER("going_second", INGAME_MULLIGAN_2ND_ID);
-    ADD_GENERATED_MARKER("victory", INGAME_VICTORY_ID);
-    ADD_GENERATED_MARKER("defeat", INGAME_DEFEAT_ID);
+    ADD_GENERATED_MARKER("victory1", INGAME_VICTORY_ID1);
+    ADD_GENERATED_MARKER("victory2", INGAME_VICTORY_ID2);
+    ADD_GENERATED_MARKER("victory3", INGAME_VICTORY_ID3);
+    ADD_GENERATED_MARKER("defeat1", INGAME_DEFEAT_ID1);
+    ADD_GENERATED_MARKER("defeat2", INGAME_DEFEAT_ID2);
+    ADD_GENERATED_MARKER("defeat3", INGAME_DEFEAT_ID3);
 
     ADD_GENERATED_MARKER("own_class_priest", INGAME_PRIEST_ME);
     ADD_GENERATED_MARKER("opponent_class_priest", INGAME_PRIEST_OPPONENT);
@@ -67,10 +71,12 @@ public:
       }
     }
     if(outcome == OUTCOME_UNKNOWN) {
-      if(FindMarker("victory")) {
+      if(FindMarker("victory1") || FindMarker("victory2") || FindMarker("victory3")) {
+        LOG("Outcome Victory %i %i %i", FindMarker("victory1"), FindMarker("victory2"), FindMarker("victory3"));
         outcome = OUTCOME_VICTORY;
       }
-      if(FindMarker("defeat")) {
+      if(FindMarker("defeat1") || FindMarker("defeat2") || FindMarker("defeat3")) {
+        LOG("Outcome Defeat %i %i %i", FindMarker("defeat1"), FindMarker("defeat2"), FindMarker("defeat3"));
         outcome = OUTCOME_DEFEAT;
       }
     }
