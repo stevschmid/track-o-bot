32cbc8b8fd1f055ed89d75ec34c96d915449b2bd
Steven Schmid <steschmid@gmx.ch>
2014-05-27 00:30:42 +0200

    improve marker detection by introducing timer

--- src/scenes/scene.h
+++ src/scenes/scene.h
@@ -5,8 +5,11 @@
 #include "../generated_markers.h"
 
 #include <QPixmap>
+#include <QElapsedTimer>
 
 #define ADD_GENERATED_MARKER(NAME, DEF) AddMarker(NAME, DEF##_PATH, DEF##_X, DEF##_Y, DEF##_WIDTH, DEF##_HEIGHT)
+
+#define DEFAULT_CONFIDENCE_LEVEL_IN_FRAMES 3
 
 class Scene
 {
@@ -30,6 +33,8 @@ private:
   map<string, Marker> markers;
   string name;
 
+  map<string, QElapsedTimer> timerForMarkers;
+
 public:
   Scene(const string& name):name(name) {
     Init();
@@ -42,11 +47,26 @@ public:
     markers[name] = Marker(name, templateImagePath, x, y, w, h);
   }
 
-  bool FindMarker(const string& name) {
+  bool FindMarker(const string& name, qint64 findIntervalInMs = 300) {
     const Marker& marker = markers[name];
     const QPixmap& capture = Hearthstone::Instance()->Capture(marker.x, marker.y, marker.w, marker.h);
     dhash currentHash = dhash_for_pixmap(capture);
-    return dhash_check_similarity(currentHash, marker.hash);
+    bool similar = dhash_check_similarity(currentHash, marker.hash);
+
+    // Due to noise and the resulting false positives
+    // make sure the marker is found for a certain amount of ms
+    // to ensure the marker is indeed the one we looked for
+    QElapsedTimer& timerForMarker = timerForMarkers[name];
+
+    if(similar) {
+      if(!timerForMarker.isValid()) {
+        timerForMarker.start();
+      }
+    } else {
+      timerForMarker.invalidate();
+    }
+
+    return timerForMarker.isValid() && timerForMarker.hasExpired(findIntervalInMs);
   }
 
   virtual bool Active() = 0;
