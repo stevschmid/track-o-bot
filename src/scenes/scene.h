74001152dfe1342949a777f8ddda2b0685237646
Steven Schmid <steschmid@gmx.ch>
2014-05-27 16:40:55 +0200

    revert the marker detection to the old system
    outcome detection was unreliable and I was not completely happy with the implementation

--- src/scenes/scene.h
+++ src/scenes/scene.h
@@ -5,7 +5,7 @@
 #include "../generated_markers.h"
 
 #include <QPixmap>
-#include <QElapsedTimer>
+#include <QDateTime>
 
 #define ADD_GENERATED_MARKER(NAME, DEF) AddMarker(NAME, DEF##_PATH, DEF##_X, DEF##_Y, DEF##_WIDTH, DEF##_HEIGHT)
 
@@ -31,8 +31,6 @@ private:
   map<string, Marker> markers;
   string name;
 
-  map<string, QElapsedTimer> timerForMarkers;
-
 public:
   Scene(const string& name):name(name) {
     Init();
@@ -45,26 +43,12 @@ public:
     markers[name] = Marker(name, templateImagePath, x, y, w, h);
   }
 
-  bool FindMarker(const string& name, qint64 findIntervalInMs = 300) {
+  bool FindMarker(const string& name) {
     const Marker& marker = markers[name];
     const QPixmap& capture = Hearthstone::Instance()->Capture(marker.x, marker.y, marker.w, marker.h);
     dhash currentHash = dhash_for_pixmap(capture);
     bool similar = dhash_check_similarity(currentHash, marker.hash);
-
-    // Due to noise and the resulting false positives
-    // make sure the marker is found for a certain amount of ms
-    // to ensure the marker is indeed the one we looked for
-    QElapsedTimer& timerForMarker = timerForMarkers[name];
-
-    if(similar) {
-      if(!timerForMarker.isValid()) {
-        timerForMarker.start();
-      }
-    } else {
-      timerForMarker.invalidate();
-    }
-
-    return timerForMarker.isValid() && timerForMarker.hasExpired(findIntervalInMs);
+    return similar;
   }
 
   virtual bool Active() = 0;
