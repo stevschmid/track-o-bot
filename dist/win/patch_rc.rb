92f0ca42ac8210b868b9fb903215fb8d4bc795d5
Steven Schmid <steschmid@gmx.ch>
2014-06-11 16:00:19 +0200

    update windows script

--- dist/win/patch_rc.rb
+++ dist/win/patch_rc.rb
@@ -5,8 +5,11 @@
 # that incorporating the auto-generated RC with the VERSION in the pro file
 # AND an icon for the executable is a use case which is "Out of scope"
 # see https://bugreports.qt-project.org/browse/QTBUG-13400
-File.open('Track-o-Bot_resource.rc', 'a') do |f|
-  f.puts "IDI_ICON1   ICON    DISCARDABLE icons/win.ico"
+
+unless File.read('Track-o-Bot_resource.rc').include?('ICON')
+  File.open('Track-o-Bot_resource.rc', 'a') do |f|
+    f.puts "IDI_ICON1   ICON    DISCARDABLE icons/win.ico"
+  end
 end
 
 # Recompile
