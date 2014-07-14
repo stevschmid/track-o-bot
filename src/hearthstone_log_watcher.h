3e2c48d7b50155e27a65cf3183bf64581cf63826
alkis <alkis@evlogimenos.com>
2014-07-14 09:40:13 +0200

    Add a comment for further development.

--- src/hearthstone_log_watcher.h
+++ src/hearthstone_log_watcher.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define CHECK_FOR_LOG_CHANGES_INTERVAL_MS 500
-
+// You can use QFileSystemWatcher here and get called as soon as Hearthstone writes to the log.
 class HearthstoneLogWatcher : public QObject
 {
   Q_OBJECT
