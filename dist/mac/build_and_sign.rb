180bbfcf3782f730330da2d6ce1c2eff90e71c02
Steven Schmid <stevschmid@gmail.com>
2016-01-14 02:01:27 +0100

    Update mac dist script

--- dist/mac/build_and_sign.rb
+++ dist/mac/build_and_sign.rb
@@ -26,40 +26,6 @@ puts `python tmp/macdeployqtfix/macdeployqtfix.py -nl -v #{APP_DIR}/Contents/Mac
 # Fix path
 `install_name_tool -change @rpath/Sparkle.framework/Versions/A/Sparkle @executable_path/../Frameworks/Sparkle.framework/Versions/A/Sparkle #{APP_DIR}/Contents/MacOS/Track-o-Bot`
 
-# What a mess with the broken macdeployqt...
-# macdeployqtfix adds new dependencies which are not fixed (i.e.
-# the Info.plist is not copied)
-# So copy them over manually
-frameworks = File.join(APP_DIR, 'Contents', 'Frameworks')
-['QtDBus.framework', 'QtPrintSupport.framework'].each do |framework|
-  path = File.join(frameworks, framework)
-  puts path
-  puts Dir.pwd
-  # fix framework structure
-  # https://developer.apple.com/library/mac/documentation/MacOSX/Conceptual/BPFrameworks/Concepts/FrameworkAnatomy.html
-  Dir.chdir(path) do
-    # Versions/Current -> Versions/4
-    lib_version = Dir.entries('Versions')[2]
-    lib = Dir.entries("Versions/#{lib_version}")[2]
-    FileUtils.ln_sf lib_version, File.join('Versions', 'Current')
-
-    # QtXXX -> Versions/Current/QtXXX
-    FileUtils.ln_sf File.join('Versions', 'Current', lib), lib
-
-    # Versions/4/Resources
-    FileUtils.mkdir 'Resources' unless Dir.exists?('Resources')
-    res_path = File.join('Versions', lib_version, 'Resources')
-    FileUtils.mv 'Resources', res_path
-    # Resources -> Versions/Current/Resources
-    FileUtils.ln_sf File.join('Versions', 'Current', 'Resources'), 'Resources'
-
-    # copy missing Info.plist
-    # Versions/4/Resources/Info.plist
-    src = File.join(QT_DIR, 'lib', framework, 'Versions', 'Current', 'Resources', 'Info.plist')
-    FileUtils.cp(src, File.join(res_path, 'Info.plist'))
-  end
-end
-
 # Codesign frameworks, plugins and app
 `codesign --verbose --deep --sign "#{SIGNATURE}" #{APP_DIR}`
 `codesign --verify --verbose=4 #{APP_DIR}`
